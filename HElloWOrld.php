c0f4bb85123e87941514f006f6309fb471f81a4c
bhk <bhk@bhk>
2017-04-20 21:46:48 +0200

    First commiit

---
+++ HElloWOrld.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$userName = "bhk";
+echo $userName;
+echo ' hellooo ';
+$currentUser = $userName;
+echo $currentUser;
+?>
